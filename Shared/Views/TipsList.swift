074b80a3738b7d134efc25f69b06799f1b59cfa6
Nicole Liu <nicoleliu_@outlook.com>
2021-01-26 08:46:43 -0500

    Added tips and map view and a tab bar to switch between them

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  TrekrMultiPlatform
+//
+//  Created by Liu, Bojun on 2021-01-26.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text("Hello, TipsList View!")
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  TrekrMultiPlatform
+//
+//  Created by Liu, Bojun on 2021-01-26.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("Hellow WorldMap View")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
